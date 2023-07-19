68e716f794e1e3ac13e082023c2eee72484a55ed
stRafa <rafael.familiamarques2016@gmail.com>
2023-07-19 17:00:22 -0300

    first commit

---
+++ LumenSoft.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LumenSoft.UI.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
